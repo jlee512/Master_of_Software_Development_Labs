ebaa195ec5b29dc98abf5efbbc610452fb67152d
jlee512 <jlee512@aucklanduni.ac.nz>
2017-09-19 15:34:57 +1200

    Cybersecurity experiment completed

---
+++ SWEN502/Code/Networking and Cybersecurity/src/Cybersecurity_Part1/RSA_KeyPairs_Part03.java
@@ -0,0 +1,26 @@
+package Cybersecurity_Part1;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Created by Julian on 19/09/2017.
+ */
+public class RSA_KeyPairs_Part03 {
+
+    public static void main(String[] args) {
+
+        try {
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+            keyPairGenerator.initialize(1024);
+            KeyPair keyPair = keyPairGenerator.generateKeyPair();
+            System.out.println("Public key: " + keyPair.getPublic());
+            System.out.println("Private key: " + keyPair.getPrivate());
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
